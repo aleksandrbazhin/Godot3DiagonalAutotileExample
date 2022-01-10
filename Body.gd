97359b7c7ddbaf85087e1823cfa54204bf14132a
aleksandrbazhin <aleksandrbazhin@gmail.com>
2022-01-10 23:00:16 +0300

    Added collisions

--- Body.gd
+++ Body.gd
@@ -4,5 +4,6 @@ var gravity = 500
 var velocity = Vector2()
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	if OS.get_ticks_msec() > 5000:
+		velocity.y += gravity * delta
+		velocity = move_and_slide(velocity, Vector2(0, -1))
