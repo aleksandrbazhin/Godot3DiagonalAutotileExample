2b794f7747577771d74128c61637669d31583023
aleksandrbazhin <aleksandrbazhin@gmail.com>
2022-11-26 09:56:03 +0700

    fix plain tile size to be 48 px, add another plain tile

--- Body.gd
+++ Body.gd
@@ -4,6 +4,6 @@ var gravity = 500
 var velocity = Vector2()
 
 func _physics_process(delta):
-	if OS.get_ticks_msec() > 5000:
+	if OS.get_ticks_msec() > 1000:
 		velocity.y += gravity * delta
 		velocity = move_and_slide(velocity, Vector2(0, -1))
